bd7699a89d8b4a22c09ee1d96c96d9a7994a0702
IDAGIZA <ruthidagiza@gmail.com>
2022-06-06 15:40:12 +0300

    finished JavaScript Function Parameters Lab

---
+++ index.js
@@ -0,0 +1,27 @@
+function introduction(name) {
+    return `Hi, my name is ${name}.`;
+  }
+
+  /*
+  function logTwoValues(value1, value2) {
+  console.log(`The two values are ${value1} and ${value2}.`);
+}
+*/
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+  /*
+
+  function sayHelloTo(firstName) {
+  console.log(`Hello, ${firstName}!`);
+}
+
+function sayHelloTo(firstName = "User") {
+  console.log(`Hello, ${firstName}!`);
+}
+*/
+
+  function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+
